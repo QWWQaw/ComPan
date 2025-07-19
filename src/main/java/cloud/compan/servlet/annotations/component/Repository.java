a62d2c1f26d05c5e438e960172f250aaa46d514a
Cheng ruixin <2066283673@qq.com>
2025-07-19 11:52:12 +0800

    开始添加controller, restcontroller注解

--- src/main/java/cloud/compan/servlet/annotations/component/Repository.java
+++ src/main/java/cloud/compan/servlet/annotations/component/Repository.java
@@ -5,15 +5,18 @@ import java.lang.annotation.*;
 /**
  * 标识数据访问层组件的注解
  *
- * <p>用于标记数据访问层的类，这些类负责与数据库交互、数据持久化操作。
- * Repository层应该封装数据访问逻辑，为上层提供干净的数据操作接口。
+ * <p>用于标记数据访问层的Repository类，这些类负责封装数据库访问逻辑，执行CRUD操作。
+ * Repository层是数据持久化的核心，提供统一的数据访问接口。
  *
  * <p>使用示例：
  * <pre>
  * {@code
  * @Repository("userRepository")
  * public class UserRepository {
- *     // 数据访问实现
+ *     public User findById(Long id) {
+ *         // 数据库查询逻辑
+ *         return user;
+ *     }
  * }
  * }
  * </pre>
@@ -22,6 +25,7 @@ import java.lang.annotation.*;
  * @since 1.0
  * @see Service
  * @see Controller
+ * @see Transactional
  */
 @Documented
 @Inherited
@@ -30,9 +34,9 @@ import java.lang.annotation.*;
 public @interface Repository {
 
     /**
-     * 指定Repository组件的名称
+     * 指定Repository的名称
      *
-     * @return Repository名称，默认为空字符串（使用类名的小驼峰形式）
+     * @return Repository的唯一标识名称，默认为空
      */
     String value() default "";
 
@@ -46,14 +50,28 @@ public @interface Repository {
     /**
      * 数据源名称
      *
-     * @return 关联的数据源名称
+     * @return 使用的数据源名称，默认为主数据源
      */
-    String dataSource() default "default";
+    String dataSource() default "primary";
 
     /**
-     * 是否启用事务管理
+     * 是否支持事务
      *
-     * @return true表示启用事务，false表示禁用
+     * @return true表示支持事务操作
      */
     boolean transactional() default true;
+
+    /**
+     * 缓存策略
+     *
+     * @return 缓存策略类型，none表示不使用缓存
+     */
+    String cacheStrategy() default "none";
+
+    /**
+     * 是否启用读写分离
+     *
+     * @return true表示启用读写分离
+     */
+    boolean readWriteSplit() default false;
 }
